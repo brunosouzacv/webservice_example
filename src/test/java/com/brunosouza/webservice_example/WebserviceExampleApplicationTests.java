725aa29e138ea1a3e7e146f7e6ba23691efc02e5
Bruno Souza <brunosouzacvlho@gmail.com>
2023-08-01 19:17:53 -0300

    Criação do projeto

---
+++ src/test/java/com/brunosouza/webservice_example/WebserviceExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brunosouza.webservice_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebserviceExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
