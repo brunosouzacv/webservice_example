7f3943b88d0f6090d05ec58f0dab2da376efbf35
Bruno Souza <brunosouzacvlho@gmail.com>
2023-08-07 19:46:53 -0300

    Enum do status do pedido

--- src/main/java/com/brunosouza/webservice_example/config/TestConfig.java
+++ src/main/java/com/brunosouza/webservice_example/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.brunosouza.webservice_example.config;
 
 import com.brunosouza.webservice_example.entities.Order;
 import com.brunosouza.webservice_example.entities.User;
+import com.brunosouza.webservice_example.enums.OrderStatus;
 import com.brunosouza.webservice_example.repositories.OrderRepository;
 import com.brunosouza.webservice_example.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,9 +25,9 @@ public class TestConfig implements CommandLineRunner {
     public void run(String... args) throws Exception {
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1, OrderStatus.PAID);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), user2, OrderStatus.CANCELED);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), user1, OrderStatus.DELIVERED);
         userRepository.saveAll(Arrays.asList(user1,user2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
     }
--- src/main/java/com/brunosouza/webservice_example/entities/Order.java
+++ src/main/java/com/brunosouza/webservice_example/entities/Order.java
@@ -1,5 +1,6 @@
 package com.brunosouza.webservice_example.entities;
 
+import com.brunosouza.webservice_example.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
@@ -19,14 +20,16 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name="client_id")
     private User client;
+    private Integer orderStatus;
 
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -51,6 +54,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
