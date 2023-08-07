d86b6dee01303ff29d8a6313e4d1f925037855e9
Bruno Souza <brunosouzacvlho@gmail.com>
2023-08-07 20:23:07 -0300

    Entidade Product

--- src/main/java/com/brunosouza/webservice_example/config/TestConfig.java
+++ src/main/java/com/brunosouza/webservice_example/config/TestConfig.java
@@ -2,10 +2,12 @@ package com.brunosouza.webservice_example.config;
 
 import com.brunosouza.webservice_example.entities.Category;
 import com.brunosouza.webservice_example.entities.Order;
+import com.brunosouza.webservice_example.entities.Product;
 import com.brunosouza.webservice_example.entities.User;
 import com.brunosouza.webservice_example.enums.OrderStatus;
 import com.brunosouza.webservice_example.repositories.CategoryRepository;
 import com.brunosouza.webservice_example.repositories.OrderRepository;
+import com.brunosouza.webservice_example.repositories.ProductRepository;
 import com.brunosouza.webservice_example.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -24,13 +26,21 @@ public class TestConfig implements CommandLineRunner {
     private OrderRepository orderRepository;
     @Autowired
     private CategoryRepository categoryRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @Override
     public void run(String... args) throws Exception {
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
+        Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+        Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+        Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+        Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+        Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
         categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+        productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
         User user1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User user2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), user1, OrderStatus.PAID);
--- src/main/java/com/brunosouza/webservice_example/entities/Category.java
+++ src/main/java/com/brunosouza/webservice_example/entities/Category.java
@@ -3,7 +3,9 @@ package com.brunosouza.webservice_example.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name="tb_category")
@@ -12,6 +14,12 @@ public class Category implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Transient
+    private Set<Product> products = new HashSet<>();
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
 
     public Category() {
     }
@@ -34,6 +42,10 @@ public class Category implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Set<Product> getProducts() {
+        return products;
     }
 
     @Override
